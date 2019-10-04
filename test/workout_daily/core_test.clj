5e1db8b6129f9eea48f1f8915c736098394c8088
ThaynanAndrey <thaynan.nunes@ccc.ufcg.edu.br>
2019-10-04 19:59:42 -0300

    Creates application boilerplate

---
+++ test/workout_daily/core_test.clj
@@ -0,0 +1,7 @@
+(ns workout-daily.core-test
+  (:require [clojure.test :refer :all]
+            [workout-daily.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
