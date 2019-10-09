92cb676d91cea8731e9bab611a84f32363167906
ThaynanAndrey <thaynan.nunes@ccc.ufcg.edu.br>
2019-10-08 22:42:18 -0300

    Creating CRUD for Exercise's entities

--- src/workout_daily/core.clj
+++ src/workout_daily/core.clj
@@ -2,10 +2,12 @@
   (:require [compojure.core :refer :all]
             [ring.middleware.json :as json]
             [workout-daily.athlete.athlete-handler :refer [athlete-handler]]
-            [workout-daily.workout.workout-handler :refer [workout-handler]]))
+            [workout-daily.workout.workout-handler :refer [workout-handler]]
+            [workout-daily.exercise.exercise-handler :refer [exercise-handler]]))
 
 (def app
      (-> (routes athlete-handler
-             workout-handler)
+                 workout-handler
+                 exercise-handler)
           (json/wrap-json-params)
           (json/wrap-json-response)))
